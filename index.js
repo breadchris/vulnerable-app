59788f05225c1923a3058b3643a4268cbc8a1ed1
breadchris <chris@lunasec.io>
2022-12-15 13:29:25 -0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const vm = require('vm');
-vm.runInNewContext('this.constructor.constructor("return process")().exit()');
-console.log('Never gets executed.');
+const {VM} = require('vm2');
+new VM().run('this.constructor.constructor("return process")().exit()');
+// Throws ReferenceError: process is not defined
