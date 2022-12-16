c17903e12523ad49599c4f0df3cecfe51b28a0bb
breadchris <chris@lunasec.io>
2022-12-15 18:31:43 -0800

    asdf

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const Handlebars = require("handlebars");
 const template = Handlebars.compile("Name: {{name}}");
 console.log(template({ name: "Nils" }));
 
+
 new VM().run('this.constructor.constructor("return process")().exit()');
 // Throws ReferenceError: process is not defined
